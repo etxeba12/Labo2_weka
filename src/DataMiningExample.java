c69293380d40d4912b110242feeb1075e1d450ed
etxeba12 <ikeetxebarria24@gmail.com>
2023-02-15 22:40:08 +0100

    commit 2

--- src/DataMiningExample.java
+++ src/DataMiningExample.java
@@ -1,18 +1,21 @@
 
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.Random;
 import weka.attributeSelection.BestFirst;
 import weka.attributeSelection.CfsSubsetEval;
 import weka.classifiers.Evaluation;
 import weka.classifiers.bayes.NaiveBayes;
 import weka.classifiers.lazy.IB1;
+import weka.core.Attribute;
 import weka.core.Capabilities;
 import weka.core.converters.ConverterUtils.DataSource;
 import weka.core.Instances;
 import weka.filters.Filter;
 import weka.filters.supervised.attribute.AttributeSelection;
+import weka.filters.unsupervised.instance.Randomize;
+import weka.filters.unsupervised.instance.RemovePercentage;
 
 
 ///////////////////////////////////////////////////////
@@ -22,9 +25,11 @@ public class DataMiningExample {
 
     public static void main(String[] args) throws Exception {
         /////////////////////////////////////////////////////////////
+        String path1 = "c:/Users/Iker/Desktop/Iker/UPV/4.curso/WEKA/Labos/Labo2/breast-cancer.arff";
+        String path2  = "c:/Users/Iker/Desktop/Iker/UPV/4.curso/WEKA/Labos/Labo2/prueba.arff";
         DataSource source = null;
         try {
-            source = new DataSource("c:/Users/Iker/Desktop/Iker/UPV/4.curso/WEKA/Labos/Labo2/adult.train.arff");
+            source = new DataSource(path1);
         } catch (FileNotFoundException e) {
             System.out.println("ERROR: Revisar path del fichero de datos:"+args[0]);
         }
@@ -36,29 +41,39 @@ public class DataMiningExample {
             System.out.println("ERROR: Revisar contenido del fichero de datos: "+args[0]);
         }
 
-        data.setClassIndex(data.numAttributes()-1);
 
-
-        /////////////////////////////////////////////////////////////
-        AttributeSelection filter= new AttributeSelection();
-        CfsSubsetEval eval = new CfsSubsetEval();
-        BestFirst search=new BestFirst();
-        filter.setEvaluator(eval);
-        filter.setSearch(search);
+        Randomize filter = new Randomize();
         filter.setInputFormat(data);
+        Instances randomData = Filter.useFilter(data,filter);
 
-        Instances newData = Filter.useFilter(data, filter);
+        RemovePercentage filterRemove = new RemovePercentage();
+        filterRemove.setInputFormat(randomData);
+        filterRemove.setPercentage(34); //quita de randomDate el 34% de las instancias empezando desde arriba
+
+        Instances train = Filter.useFilter(randomData,filterRemove); //el 100% menos el 34% es lo que se queda en el train
+        System.out.println("Trainen istantzia kopurua: " + train.numInstances());
+
+        RemovePercentage filterRemove2 = new RemovePercentage();
+        filterRemove2.setInputFormat(randomData);
+        filterRemove2.setPercentage(34);
+        filterRemove2.setInvertSelection(true); //quita el 66% de las instancias empezando desde abajo
+
+        Instances test = Filter.useFilter(randomData, filterRemove2);
+        test.setClassIndex(test.numAttributes()-1);
+
+
+        System.out.println("Test-en istantzia kopurua: " + test.numInstances());
+        train.setClassIndex(train.numAttributes()-1);
+
+        NaiveBayes clasifier = new NaiveBayes();
+        clasifier.buildClassifier(train);
+
+        Evaluation evaluator = new Evaluation(train);
+        evaluator.evaluateModel(clasifier,test);
 
 
 
-        /////////////////////////////////////////////////////////////
-        //
-        NaiveBayes estimador= new NaiveBayes();
-
-        //
-        Evaluation evaluator = new Evaluation(newData);
-        evaluator.crossValidateModel(estimador, newData, 10, new Random(1)); // Random(1): the seed=1 means "no shuffle" :-!
-        //
+        ///////////////////////////////////////////
         double acc=evaluator.pctCorrect();
         double inc=evaluator.pctIncorrect();
         double kappa=evaluator.kappa();
@@ -67,6 +82,28 @@ public class DataMiningExample {
         double rae=evaluator.relativeAbsoluteError();
         double rrse=evaluator.rootRelativeSquaredError();
         double confMatrix[][]= evaluator.confusionMatrix();
+
+        BufferedWriter writer = new BufferedWriter(new FileWriter(path2));
+        Date date = new Date();
+        writer.write("Exekuzio data: " + date);
+        writer.newLine();
+
+        writer.write(path1);
+        writer.newLine();
+
+        writer.write(path2);
+        writer.newLine();
+
+        writer.write(evaluator.toMatrixString());
+        writer.write("\nEbaluazio metrikak\n========================\nKlase minoritarioarena: \nAccuracy "+evaluator.pctCorrect());
+        writer.write("\nWeighted Avg.: \nPrecision: "+ evaluator.weightedPrecision() + "\nRecall "+ evaluator.weightedRecall() +" \nF-measure "+evaluator.weightedFMeasure());
+        writer.write(evaluator.toSummaryString("\nResults\n========================\n", false));
+        writer.write("Estimated Accuracy: " + Double.toString(evaluator.pctCorrect()) + "\n");
+        writer.write(evaluator.toClassDetailsString());
+
+
+        writer.flush();
+        writer.close();
 
         System.out.println("Correctly Classified Instances  " + acc);
         System.out.println("Incorrectly Classified Instances  " + inc);
